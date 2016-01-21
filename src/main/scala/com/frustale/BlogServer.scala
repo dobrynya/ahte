52523c74758114574f5c0162cb0aab59d149231f
Dmitry Dobrynin <dobrynya@inbox.ru>
2016-01-21 14:18:14 +0300

    Changes.

--- src/main/scala/com/frustale/BlogServer.scala
+++ src/main/scala/com/frustale/BlogServer.scala
@@ -2,6 +2,7 @@ package com.frustale
 
 import java.time._
 import java.time.format.DateTimeFormatter
+import java.util.UUID
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
@@ -11,8 +12,10 @@ import spray.json._
 
 case class Blog(name: String, author: String)
 
-case class Post(title: String, created: LocalDateTime, content: String, modified: Option[LocalDateTime] = None) {
-  def onlyThreeSentences = {
+case class Post(title: String, created: LocalDateTime, content: String, modified: Option[LocalDateTime] = None,
+                id: String = UUID.randomUUID().toString) {
+
+  def only3Sentences = {
     def findNextSentence(currentPos: Int, numberOfSentences: Int, maxSentences: Int): Int =
       if (numberOfSentences == maxSentences || currentPos == content.length)
         currentPos
@@ -25,11 +28,54 @@ case class Post(title: String, created: LocalDateTime, content: String, modified
   }
 }
 
-case class Comment(author: String, created: LocalDateTime, content: String)
+case class Comment(author: String, created: LocalDateTime, content: String, id: String = UUID.randomUUID().toString)
+
+trait BlogStore {
+  var blog = Blog("Dmitry's blog", "Dmitry")
+
+  var posts = {
+    val post = Post("First post", LocalDateTime.now, "Content with four sentences. The second one. The third! The fourth?")
+    Map(post.id -> post)
+  }
+
+  var comments = Map.empty[String, List[Comment]]
+
+  def blogPosts = BlogPosts(blog.name, blog.author, posts.values.map(_.only3Sentences).toList)
+
+  def createPost(title: String, content: String): Post = {
+    val post = Post(title, LocalDateTime.now, content)
+    posts += post.id -> post
+    post
+  }
+
+  def updatePost(id: String, title: Option[String], content: Option[String]): Option[Post] = {
+    for (old <- posts.get(id))
+      yield {
+        val post = old.copy(title = title.getOrElse(old.title), content = content.getOrElse(old.content))
+        posts += post.id -> post
+        post
+      }
+  }
+
+  def deletePost(id: String): Option[Post] =
+    for (old <- posts.get(id)) yield {
+      posts -= id
+      old
+    }
+
+  def postComments(id: String): Option[PostComments] =
+    for (post <- posts.get(id))
+      yield PostComments(blog.name, blog.author, post.title, comments.getOrElse(id, Nil))
+}
+
 
 case class BlogPosts(name: String, author: String, posts: List[Post])
 
 case class PostComments(blog: String, author: String, title: String, comments: List[Comment])
+
+case class PutPost(title: String, content: String)
+
+case class PatchPost(title: Option[String], content: Option[String])
 
 trait BlogProtocol extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val dateFormat = new JsonFormat[LocalDateTime] {
@@ -39,10 +85,12 @@ trait BlogProtocol extends SprayJsonSupport with DefaultJsonProtocol {
   }
 
   implicit val blogFormat = jsonFormat2(Blog)
-  implicit val postFormat = jsonFormat4(Post)
-  implicit val commentFormat = jsonFormat3(Comment)
+  implicit val postFormat = jsonFormat5(Post)
+  implicit val commentFormat = jsonFormat4(Comment)
   implicit val postsFormat = jsonFormat3(BlogPosts)
   implicit val postComments = jsonFormat4(PostComments)
+  implicit val putPostFormat = jsonFormat2(PutPost)
+  implicit val patchPost = jsonFormat2(PatchPost)
 }
 
 /**
@@ -50,26 +98,37 @@ trait BlogProtocol extends SprayJsonSupport with DefaultJsonProtocol {
  * @author Dmitry Dobrynin <dmitry.v.dobrynin@gmail.com>
  *         Created at 20.01.2016 13:53
  */
-object BlogServer extends App with BlogProtocol {
+object BlogServer extends App with BlogProtocol with BlogStore {
   implicit val system = ActorSystem("my-system")
   implicit val materializer = ActorMaterializer()
   implicit val ec = system.dispatcher
 
   val route =
-    get {
-      pathEndOrSingleSlash {
-        complete {
-          BlogPosts("Blog Name", "Dmitry",
-            List(Post("Fucking test!", LocalDateTime.now(),
-              "I don't like this test! Because it sucks. How much time I have? I don't know."))
-              .map(_.onlyThreeSentences)
-          )
+    pathEndOrSingleSlash {
+      get {
+        complete(blogPosts)
+      }
+    } ~
+    put {
+      path("/post") {
+        entity(as[PutPost]) { post =>
+          complete(createPost(post.title, post.content))
         }
+      }
+    } ~
+    path("post" / Segment) { id =>
+      get {
+        complete(postComments(id))
       } ~
-      path("posts")
+      delete {
+        complete(deletePost(id))
+      } ~
+      patch {
+        entity(as[PatchPost]) { patchPost =>
+          complete(updatePost(id, patchPost.title, patchPost.content))
+        }
+      }
     }
-
-}
 
   val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
